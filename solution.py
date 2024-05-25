d554deca31603e22c7938f0137fe6b5229987a48
Alexander Osipov <alexander.osipov.work@gmail.com>
2024-05-25 20:54:37 +0700

    Add: unit tests

---
+++ solution.py
@@ -0,0 +1,27 @@
+import re
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+
+        try:
+            self.__checkInput__(s)
+        except Exception as e:
+            raise e
+
+        for i in range(len(s)):
+            res += self.__countPali__(s, i, i)
+            res += self.__countPali__(s, i, i + 1)
+        return res
+
+    def __countPali__(self, s, l, r):
+        res = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            res += 1
+            l -= 1
+            r += 1
+        return res
+
+    def __checkInput__(self,s):
+        if not(type(s) is str): raise TypeError(f"Incorrect input string type:${type(s)} expected ${type("")}")
+        if not(re.fullmatch(r'^[a-z]+$',s)): raise AssertionError('Input string must contain only lowercases en letters')
+        if not(len(s) > 0 and len(s) <= 1000): raise AssertionError('Input string length must be between 0 and 1001')
---
+++ tests/unit/test_solution.py
@@ -0,0 +1,92 @@
+import unittest
+from unittest import TestCase
+from solution import Solution
+
+
+
+class TestSolution(TestCase):
+
+    def setUp(self) -> None:
+        self.sol = Solution()
+
+    def test_palindromic_substrings_count__correct_result(self):
+        data = [['abc', 3], ['aaa', 6]]
+        for s in data:
+            with self.subTest(string=s[0], right_count=s[1]):
+                self.assertEqual(self.sol.countSubstrings(s[0]), s[1])
+
+    def test_palindromic_substrings_count__incorrect_result(self):
+        data = [['abca', 3], ['abcb', 3]]
+        for s in data:
+            with self.subTest(string=s[0], wrong_count=s[1]):
+                self.assertNotEqual(self.sol.countSubstrings(s[0]), s[1])
+
+    def test_palindromic_substrings_count__input_string_length_valid(self):
+        min_input_string_length = 1
+        max_input_string_length = 1000
+        data = {
+            "left_on_bound": "a",
+            "left_gt_bound": "aa",
+            "right_on_bound": "a" * max_input_string_length,
+            "right_lt_bound": "a" * (max_input_string_length - min_input_string_length),
+        }
+        #Сделал так потому что в логике проверки можем поломать проверочные значения и на корректные входные данные вылетит ex
+        for s in data:
+            with self.subTest(input_string_type=s, input_string_length=len(data[s])):
+                try:
+                    self.sol.countSubstrings(data[s])
+                except Exception as e:
+                    self.fail(e)
+
+    def test_palindromic_substrings_count__input_string_length_invalid(self):
+        min_input_string_length = 1
+        max_input_string_length = 1000
+        data = {
+            "left_lt_bound": "",
+            "right_gt_bound": "a" * (max_input_string_length + min_input_string_length),
+        }
+        for s in data:
+            with self.subTest(input_string_type=s, input_string_length=len(data[s])):
+                self.assertRaises(AssertionError, self.sol.countSubstrings, data[s])
+
+    def test_palindromic_substrings_count__input_string_type_invalid(self):
+        cases = [
+            123,
+            12.3,
+            ['a', 'b', 'c'],
+            {'a': 1, 'b': 2},
+            None
+        ]
+        for s in cases:
+            with self.subTest(input_string_type=type(s)):
+                self.assertRaises(TypeError, self.sol.countSubstrings, s)
+
+    def test_palindromic_substrings_count__input_string_value_invalid(self):
+        cases = [
+            '123',
+            "123abc123",
+            "abc123abc",
+            'ABC',
+            "abcABC",
+            "abcABCabc",
+            "ABCabcABC",
+            "абц",
+            "абцabcабц",
+            "abcабцabc",
+            '異體字',
+            "異體字abc異體字",
+            "abc異體字abc",
+            " ",
+            "abc ",
+            " abc",
+            "abc abc",
+            "@#!",
+            "!@#abc!@#",
+            "abc!@#abc"
+        ]
+        for s in cases:
+            with self.subTest(incorrect_input_value=s):
+                self.assertRaises(AssertionError, self.sol.countSubstrings, s)
+
+if __name__ == '__main__':
+    unittest.main()
